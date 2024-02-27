9c1104c233e005b56a3f9c578441a725659008bc
Hany Darwish <hany.freestyle4u@gmail.com>
2024-02-27 07:53:10 +0200

    Sours

--- resources/views/components/site/def/breadcrumbs.blade.php
+++
@@ -1,9 +0,0 @@
-<div class=" bg-primary-5p">
-    <div class="container ">
-        <div class="row">
-            <div class="col-lg-12">
-                {{$slot}}
-            </div>
-        </div>
-    </div>
-</div>
