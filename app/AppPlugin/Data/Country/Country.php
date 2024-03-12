b219fed76e496ae6fb4291a85c30b87333a37112
Hany Darwish <hany.freestyle4u@gmail.com>
2024-03-12 02:37:52 +0200

    update

--- app/AppPlugin/Data/Country/Country.php
+++ app/AppPlugin/Data/Country/Country.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
-class BrandCountry extends Model implements TranslatableContract {
+class Country extends Model implements TranslatableContract {
 
   use SoftDeletes;
   use Translatable;
